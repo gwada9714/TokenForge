0fcacaf34c7f10d501641ea2b93aee5554b45b40
Gouedard <gouedard.s@live.fr>
2025-01-12 00:06:47 +0100

    Update dependencies and configuration files

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,3 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
+import { Layout } from './components/Layout';
 
-import { theme } from './theme';
-import Layout from './components/Layout';
-
-const App: React.FC = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Layout>
-        <Container maxWidth="lg">
-          <Box sx={{ my: 4 }}>
-            <Routes>
-              <Route path="/" element={<div>TokenForge - Créateur de Tokens</div>} />
-            </Routes>
-          </Box>
-        </Container>
-      </Layout>
-    </ThemeProvider>
-  );
-};
-
-export default App;
+// Le reste de votre code App.tsx ici
--- vite.config.js
+++ vite.config.js
@@ -24,6 +24,23 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: true
-  }
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'mui-vendor': ['@mui/material', '@mui/icons-material'],
+          'web3-vendor': ['ethers', 'web3', 'wagmi', '@wagmi/core', 'viem'],
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
+    exclude: [
+      'vite-plugin-node-polyfills/shims/buffer',
+      'vite-plugin-node-polyfills/shims/global',
+      'vite-plugin-node-polyfills/shims/process'
+    ],
+  },
 });
