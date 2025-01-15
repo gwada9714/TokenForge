78408b4b71a7eaf1b2bb4bca482f05ce7fcb41e7
Gouedard <gouedard.s@live.fr>
2025-01-15 01:54:12 +0100

    feat: update smart contracts and related scripts

---
+++ scripts/generate-types.ts
@@ -0,0 +1,16 @@
+import { runTypeChain, glob } from 'typechain';
+
+async function main() {
+  const cwd = process.cwd();
+  const files = glob(cwd, ['./artifacts/contracts/**/*.json']);
+
+  await runTypeChain({
+    cwd,
+    filesToProcess: files,
+    allFiles: files,
+    outDir: 'src/typechain',
+    target: 'ethers-v6',
+  });
+}
+
+main().catch(console.error); 
