63827ca327401471c006e00dea2f8be2404b622c
TokenForge Developer <dev@tokenforge.local>
2025-01-15 19:32:14 +0100

    refactor: update App component to use Material-UI providers
    - Replace ChakraProvider with ThemeProvider
    - Replace CSSReset with CssBaseline
    - Update imports from @chakra-ui/react to @mui/material

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import Header from './components/Header/Header';
 import { Toaster } from 'react-hot-toast';
 import AppRouter from './components/Router/AppRouter';
 import { UserPlanProvider } from './contexts/UserPlanContext';
-import { ChakraProvider, CSSReset } from '@chakra-ui/react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
 import { forgeTheme } from './theme/forge-theme';
 
 function App() {
   return (
-    <ChakraProvider theme={forgeTheme}>
-      <CSSReset />
+    <ThemeProvider theme={forgeTheme}>
+      <CssBaseline />
       <UserPlanProvider>
         <div className="min-h-screen bg-gray-50 flex flex-col">
           <Header />
@@ -18,7 +18,7 @@ function App() {
           <Toaster position="bottom-right" />
         </div>
       </UserPlanProvider>
-    </ChakraProvider>
+    </ThemeProvider>
   );
 }
 
