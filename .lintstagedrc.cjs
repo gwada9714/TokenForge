bbdaa02eeddf56d0404633a2d323278bb417cda4
TokenForge Developer <dev@tokenforge.local>
2025-01-16 20:00:24 +0100

    chore: update lint-staged and husky configuration

---
+++ .lintstagedrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  '*.{js,jsx,ts,tsx}': files => [
+    `eslint --fix ${files.join(' ')}`,
+    `prettier --write ${files.join(' ')}`
+  ],
+  '*.sol': files => [
+    `prettier --write ${files.join(' ')}`,
+    `solhint ${files.join(' ')}`
+  ],
+  '*.{json,md,yml}': files => [
+    `prettier --write ${files.join(' ')}`
+  ]
+}
