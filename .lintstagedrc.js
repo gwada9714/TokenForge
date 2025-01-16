a09611b77713b0dd96261670dcd7519ae278ba45
TokenForge Developer <dev@tokenforge.local>
2025-01-16 20:03:37 +0100

    chore(config): clean up old configuration files

--- .lintstagedrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  '*.{js,jsx,ts,tsx}': [
-    'eslint --fix',
-    'prettier --write'
-  ],
-  '*.sol': ['prettier --write', 'solhint'],
-  '*.{json,md,yml}': ['prettier --write']
-}
