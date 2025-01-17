04d42f442673586ff164c3a32837d9fd97c38884
TokenForge Developer <dev@tokenforge.local>
2025-01-17 04:30:24 +0100

    feat: add lazy loading components and utilities

---
+++ src/components/common/LoadingFallback.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { CircularProgress } from '@mui/material';
+import styled from 'styled-components';
+
+const LoadingContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 200px;
+  width: 100%;
+`;
+
+const LoadingFallback: React.FC = () => (
+  <LoadingContainer>
+    <CircularProgress />
+  </LoadingContainer>
+);
+
+export default LoadingFallback;
---
+++ src/components/common/SuspenseWrapper.tsx
@@ -0,0 +1,14 @@
+import React, { Suspense } from 'react';
+import LoadingFallback from './LoadingFallback';
+
+interface SuspenseWrapperProps {
+  children: React.ReactNode;
+}
+
+const SuspenseWrapper: React.FC<SuspenseWrapperProps> = ({ children }) => (
+  <Suspense fallback={<LoadingFallback />}>
+    {children}
+  </Suspense>
+);
+
+export default SuspenseWrapper;
