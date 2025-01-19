7017c43adefeec8cf7100d0d7666850d6a17d43a
TokenForge Developer <dev@tokenforge.local>
2025-01-19 06:25:08 +0100

    refactor: update type definitions and index files

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,2 +1,2 @@
-export { default as Providers } from './Providers';
-export { useWeb3, Web3Provider } from './Web3Provider';
+export * from './contract';
+export * from './web3';
--- src/types/window.d.ts
+++ src/types/window.d.ts
@@ -4,6 +4,17 @@ interface Workbox {
   // Add other workbox methods you're using
 }
 
-declare interface Window {
+interface Window {
   workbox: Workbox;
+  ethereum?: {
+    request: (args: { method: string; params?: any[] }) => Promise<any>;
+    on: (eventName: string, handler: (...args: any[]) => void) => void;
+    removeListener: (eventName: string, handler: (...args: any[]) => void) => void;
+    isMetaMask?: boolean;
+    isConnected: () => boolean;
+    chainId: string;
+    networkVersion: string;
+  };
 }
+
+export {};
